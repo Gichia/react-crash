9f9e67b0e6dec0fbe585dc06d2ed4dc4293463f7
Gichia <petergichia35@gmail.com>
2019-04-25 12:56:21 +0300

    Updates

---
+++ precache-manifest.ccc3097da86a874a748cff04ad8507d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16cd572b98e15185591f",
+    "url": "/react-crash/static/css/main.38381bac.chunk.css"
+  },
+  {
+    "revision": "16cd572b98e15185591f",
+    "url": "/react-crash/static/js/main.16cd572b.chunk.js"
+  },
+  {
+    "revision": "f0398fb05c1abe3864af",
+    "url": "/react-crash/static/js/1.f0398fb0.chunk.js"
+  },
+  {
+    "revision": "3ca83d63cc075330b842",
+    "url": "/react-crash/static/js/runtime~main.3ca83d63.js"
+  },
+  {
+    "revision": "5905962fdecbfc07590c1f87e49278a1",
+    "url": "/react-crash/index.html"
+  }
+];
